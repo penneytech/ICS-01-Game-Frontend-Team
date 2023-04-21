3c9b85aaefbee8bcf57627279fbde29411c76873
cooluser1337 <130511088+cooluser1337@users.noreply.github.com>
2023-04-21 14:45:25 +0000

    Added Joystick

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // This is the entrypoint file that ties every other file together, and calls the initial modules
 
 // Run the socket module to establish a connection with the server
-// import './socket/socket.js';
-// import './sampledata.js';
+ import './socket/socket.js';
+
+//Import the joystick
+ import './joystick/joystick.js'
 
 /* Create the login dialogue splash screen. For DEVELOPMENT purposes, you can comment this import line out and instead import any other pages you want to run. For example, to instead initialize the canvas testing page, comment out the line below and uncomment the other two lines */
 // import './pages/loginDialogue.js'
